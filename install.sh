4095787680cb42d258cead6fac79d7423178dfae
TheiLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2023-04-11 08:59:49 +0000

    Add output convars for server.cfg

--- install.sh
+++ install.sh
@@ -9,14 +9,23 @@
 
 if [[ "$(config_val 'baseDomain')" == "" ]]; then
   IP=$(ip -f inet addr show eth0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
-  LOKI_URL="http://$IP:$(config_val 'loki.exposedPort')"
-  GRAFANA_URL="http://$IP:$(config_val 'grafana.exposedPort')"
+  LOKI_URL="$IP:$(config_val 'loki.exposedPort')"
+  GRAFANA_URL="$IP:$(config_val 'grafana.exposedPort')"
+  PROTOCOL="http"
 else
-  LOKI_URL="https://loki.$(config_val 'baseDomain')"
-  GRAFANA_URL="https://grafana.$(config_val 'baseDomain')"
+  LOKI_URL="loki.$(config_val 'baseDomain')"
+  GRAFANA_URL="grafana.$(config_val 'baseDomain')"
+  PROTOCOL="https"
 fi
 {
  printf 'Service\tURL\tUsername\tPassword\n';
- printf '%s\t%s\t%s\t%s\n' "Loki" $LOKI_URL $(config_val "loki.username") $(config_val "loki.password");
- printf '%s\t%s\t%s\t%s\n' "Grafana" $GRAFANA_URL $(config_val "grafana.username") $(config_val "grafana.password");
+ printf '%s\t%s\t%s\t%s\n' "Loki" ${PROTOCOL}://${LOKI_URL} $(config_val "loki.username") $(config_val "loki.password");
+ printf '%s\t%s\t%s\t%s\n' "Grafana" ${PROTOCOL}://${GRAFANA_URL} $(config_val "grafana.username") $(config_val "grafana.password");
 } | prettytable 4
+
+printf "You can now add the following to your server.cfg in order to configure ox_lib Logger:\n\n"
+echo "set ox:logger \"loki\""
+echo "set loki:user \"$(config_val 'loki.username')\""
+echo "set loki:key \"$(config_val 'loki.password')\""
+echo "set loki:key \"$PROTOCOL\""
+echo "set loki:endpoint \"$LOKI_URL\""
